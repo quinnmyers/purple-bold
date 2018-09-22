047bf7d875750f4ad34a0bfdb4547c8226cea5c5
Quinn <quinnsmyers@gmail.com>
2018-09-21 22:53:56 -0700

    added landing component

--- purple-bold/src/App.vue
+++ purple-bold/src/App.vue
@@ -1,20 +1,30 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    app-landing
+
+  
+  
+  
+  //#app
+    #nav 
+      router-link(to='/') Home | 
+      router-link(to='/about') About
+      router-view
+
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  components: {
+    appLanding: Landing
+  }
 }
+</script>
+
+<style lang="sass">
+
+
+
 </style>
---
+++ purple-bold/src/components/Landing.vue
@@ -0,0 +1,91 @@
+<template lang="pug">
+    div
+      .landing__page
+        .landing__page__content
+          .landing__page__content__container
+            .landing__page__content__container__top
+              h1 Purple
+            .landing__page__content__container__middle
+              .landing__page__content__container__middle__left
+              h1 +
+              .landing__page__content__container__middle__right
+              h1 Bold
+            .landing__page__content__container__bottom
+              img.landing--arrow(src='../assets/images/icons/arrow.svg')
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="sass">
+@import '../normalize.scss'
+@import '../base.sass'
+
+$arrow-margin-bottom: 20px 
+
+.landing__page
+  display: flex
+  flex-direction: column
+  width: 100vw
+  height: 100vh
+  background: $blue-grey
+  font-family: 'Rubik', 'Avenir', sans-serif
+  &__content
+    display: flex
+    flex-direction: column
+    align-self: center
+    align-items: flex-end
+    width: 1100px
+    margin: auto 0
+    color: white
+    &__container
+      display: flex
+      flex-direction: column
+      height: 100vh
+      justify-content: center
+      &__top h1
+        font-size: 7em
+        letter-spacing: .15em
+        margin-right: -.15em
+        font-weight: $light
+      &__middle
+        display: flex
+        justify-content: space-between
+        h1
+          font-size: 6em
+        &__left 
+        &__right
+      &__bottom
+        display: flex
+        align-self: flex-end
+        position: absolute
+        bottom: 0
+        justify-content: flex-end
+        img
+          height: 70px
+          margin-bottom: $arrow-margin-bottom
+          animation: arrow 4s ease-in-out infinite
+
+@keyframes arrow
+  0%
+  10%
+  20%
+  30%
+  40%
+  50%
+    transform: translateY(-20px)
+  60%
+  70%
+  80% 
+  90%
+  100%
+    
+  
+
+
+</style>
+
+
