4c369e4c71d91589ac8e7edaf08d0b02095f2f0f
Quinn <quinnsmyers@gmail.com>
2018-09-22 00:34:00 -0700

    added some styling for landing text animations

--- purple-bold/src/components/Landing.vue
+++ purple-bold/src/components/Landing.vue
@@ -1,22 +1,40 @@
 <template lang="pug">
     div
       .landing__page
+        button(
+          @click="toggleLoad"
+        ) Click Me
         .landing__page__content
           .landing__page__content__container
-            .landing__page__content__container__top
+            .landing__page__content__container__top(
+              :class="{loaded: isLoaded}"
+              )
               h1 Purple
             .landing__page__content__container__middle
-              .landing__page__content__container__middle__left
-              h1 +
-              .landing__page__content__container__middle__right
-              h1 Bold
+              .landing__page__content__container__middle__left(
+                :class="{loaded: isLoaded}"
+              )
+                h1 +
+              .landing__page__content__container__middle__right(
+                :class="{loaded: isLoaded}"
+              )
+                h1 Bold
             .landing__page__content__container__bottom
               img.landing--arrow(src='../assets/images/icons/arrow.svg')
 </template>
 
 <script>
-    export default {
-        
+   export default {
+        data() {
+          return {
+            isLoaded: false
+          }
+        },
+        methods: {
+          toggleLoad() {
+            this.isLoaded = !this.isLoaded;
+          }
+        }
     }
 </script>
 
@@ -46,18 +64,31 @@ $arrow-margin-bottom: 20px
       flex-direction: column
       height: 100vh
       justify-content: center
-      &__top h1
-        font-size: 7em
-        letter-spacing: .15em
-        margin-right: -.15em
-        font-weight: $light
+      &__top.loaded
+        animation:
+        transform: translateX(0px)
+      &__top
+        margin-bottom: 10px
+        text-align: right
+        transform: translateX(-100vw)
+        h1 
+          font-size: 7em
+          letter-spacing: .15em
+          margin-right: -.15em
+          font-weight: $light
       &__middle
         display: flex
         justify-content: space-between
         h1
           font-size: 6em
-        &__left 
+        &__left.loaded
+          transform: translateX(0px)
+        &__left
+          transform: translateX(-100vw)
+        &__right.loaded
+          transform: translateX(0px)
         &__right
+          transform: translateX(-100vw)
       &__bottom
         display: flex
         align-self: flex-end
@@ -67,9 +98,9 @@ $arrow-margin-bottom: 20px
         img
           height: 70px
           margin-bottom: $arrow-margin-bottom
-          animation: arrow 4s ease-in-out infinite
+          animation: landing-arrow 4s ease-in-out infinite
 
-@keyframes arrow
+@keyframes landing-arrow
   0%
   10%
   20%
@@ -83,6 +114,47 @@ $arrow-margin-bottom: 20px
   90%
   100%
     
+
+@keyframes landing-top-load
+  0%
+  10%
+  20%
+  30%
+  40%
+  50%
+  60%
+  70%
+  80% 
+  90%
+  100%
+
+@keyframes landing-middle-load
+  0%
+  10%
+  20%
+  30%
+  40%
+  50%
+  60%
+  70%
+  80% 
+  90%
+  100%
+
+  @keyframes landing-bottom-load
+  0%
+  10%
+  20%
+  30%
+  40%
+  50%
+  60%
+  70%
+  80% 
+  90%
+  100%
+  
+  
   
 
 
