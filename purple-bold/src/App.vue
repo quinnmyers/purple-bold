524bf1d65ff1f6d1e2310eadfb5bd432b793f830
Quinn <quinnsmyers@gmail.com>
2018-09-30 15:03:44 -0700

    reset to 5a423cb

--- purple-bold/src/App.vue
+++ purple-bold/src/App.vue
@@ -1,24 +1,26 @@
-<template>
-  <div id="app">
-    <app-landing></app-landing>
-    <app-navbar></app-navbar>
-    <app-hero></app-hero>
-    <app-about></app-about>
-  <div>
+<template lang="pug">
+  #app
+    app-landing
+    app-navbar
+    app-hero
+    app-about
 
-  <!-- //#app
+  
+  
+  
+  //#app
     #nav 
       router-link(to='/') Home | 
       router-link(to='/about') About
-      router-view -->
+      router-view
 
 </template>
 
 <script>
-import Landing from "./components/Landing.vue";
-import Navbar from "./components/Navbar/Navbar.vue";
-import Hero from "./components/Hero.vue";
-import About from "./components/About/About.vue";
+import Landing from './components/Landing.vue'
+import Navbar from './components/Navbar/Navbar.vue'
+import Hero from './components/Hero.vue'
+import About from './components/About/About.vue'
 
 export default {
   components: {
@@ -27,7 +29,7 @@ export default {
     appHero: Hero,
     appAbout: About
   }
-};
+}
 </script>
 
 <style lang="sass">
