9eca864c26844af0ac8388b6e82103d73d349225
Quinn <quinnsmyers@gmail.com>
2018-10-14 20:47:10 -0700

    added some functionality to load-in portfolio grid, may not use it, added tagging and filtering functionality with animations

--- purple-bold/src/components/Portfolio/PortfolioGrid.vue
+++ purple-bold/src/components/Portfolio/PortfolioGrid.vue
@@ -24,7 +24,8 @@
     <div class="portfolio__grid__container">
       <template v-for="(piece,index) in filteredPortfolioPieces">
         <!-- <template v-if="categoryArray.includes(piece.type.toLowerCase())"> -->
-          <div :class="[`item portfolio__grid__container__item`, piece.type.toLowerCase()]">
+          <div :class="[`item portfolio__grid__container__item`, piece.type.toLowerCase(), `{is-visible: isVisible}`]"
+               :ref="`portfolio-grid-item`">
             <h4>{{ piece.type }}</h4>
             <p>{{ piece.description.substr(0, 99)  }}</p>
           </div>
@@ -49,26 +50,37 @@ export default {
     return {
       isMounted: false,
       categoryArray: [],
-      selectedCategories: []
+      selectedCategories: [],
+      isVisible: true
     };
   },
   methods: {
-    populateCategoryArray() {
-      const limit = this.tagArray.length;
-      let counter = 0;
-      while (counter < limit) {
-        this.categoryArray.push(this.tagArray[counter].name);
-        console.log("pushing: " + this.tagArray[counter].name);
-        counter++;
-      }
-      console.log(this.categoryArray);
-    }
+    // loadPanels() {
+    //   //const limit = this.$refs["portfolio-grid-item"].length;
+    //   //console.log(this.$refs["portfolio-grid-item"][0]);
+    //   const itemArray = [...this.$refs["portfolio-grid-item"]];
+    //   console.log(itemArray);
+    //   let index = 0;
+    //   itemArray.forEach(function(i) {
+    //     const el = itemArray[index];
+    //     setTimeout(() => {
+    //       el.classList.add("is-visible");
+    //     }, index * 100);
+    //     index++;
+    //   });
+    // }
+    // console.log("grid refs: ", this.$refs["portfolio-grid-item"][2]);
   },
   computed: {
+    //provides the grid with the list of items to display
     filteredPortfolioPieces() {
+      //checks to see if nothing is selected (selectedCategories array is empty)
       if (this.selectedCategories.length === 0) {
+        //if nothing is selected, it shows the entire array unfiltered
         return this.portfolioPieces;
       } else {
+        //if there are selections, it filters the array for each category inside of the
+        //selectedCategories array and checks it against the piece.type of the portfolio piece
         return this.portfolioPieces.filter(piece =>
           this.selectedCategories.includes(piece.type.toLowerCase())
         );
@@ -76,21 +88,23 @@ export default {
     }
   },
   mounted() {
-    this.populateCategoryArray();
-    this.$nextTick(() => {
-      this.isMounted = true;
-    });
+    this.isMounted = true;
+    // this.$nextTick(this.loadPanels);
+    //populating selectedCategory array
     eventBus.$on("categoryWasChanged", category => {
-      console.log("grid speaking, category is: " + category);
+      //checks to see if selectedCategories array includes selection (tag button)
       if (this.selectedCategories.includes(category)) {
-        this.selectedCategories = this.selectedCategories.filter(
+        //if the array already includes the category (it's selected) then it
+        //filters over the array to remove that category
+        return (this.selectedCategories = this.selectedCategories.filter(
           item => item != category
-        );
-        console.log(this.selectedCategories);
-        return;
+        ));
       } else {
-        this.selectedCategories.push(category);
-        console.log(this.selectedCategories);
+        //if the array doesn't already include that category selection, it
+        //adds that category to the array
+        setTimeout(() => {
+          this.selectedCategories.push(category);
+        }, 268.75);
       }
     });
   }
@@ -112,23 +126,34 @@ export default {
 
 <style scoped lang="sass">
 .portfolio__grid
-  margin-top: 60px
+  margin-top: 20px
   &__container
     display: grid 
-    grid-gap: 14px
-    grid-template-columns: repeat(auto-fill, minmax(264px,1fr))
-    grid-auto-rows: 100px
+    grid-gap: 20px
+    grid-template-columns: repeat(auto-fill, minmax(254px,1fr))
+    grid-auto-rows: 43px
     &__item 
-      border: 1px solid red
-      width: 264px
+      //border: 1px solid red
+      opacity: 1
+      box-shadow: 0px 0px 10px 1px rgba(218,218,218,1);
+      border-radius: 4px
+      width: 258px
+      transition: all .3s ease-in-out
+      &.is-visible 
+        display: flex
+        opacity: 1
+        flex-direction: column
+        transform: rotate(0deg) scale(1)
+      &:hover 
+        transform: scale(1.01)
       &.website 
-        grid-row-end: span 3
+        grid-row-end: span 6
       &.logo
-        grid-row-end: span 1
+        grid-row-end: span 3
       &.graphic 
-        grid-row-end: span 2
+        grid-row-end: span 3
       &.branding
-        grid-row-end: span 1
+        grid-row-end: span 4
 
   // .masonry-container 
   //   width: 55%
--- purple-bold/src/components/Portfolio/PortfolioNav.vue
+++ purple-bold/src/components/Portfolio/PortfolioNav.vue
@@ -1,8 +1,9 @@
 <template>
-  <ul>
+  <ul v-if="isMounted">
     <li
       v-for="(tag, index) in tagArray"
-      @click="categorySelected(index)"> 
+      @click="categorySelected(index)"
+      :ref="`tag-button-${index}`"> 
       {{ tag.name }}
     </li>
   </ul>
@@ -13,17 +14,29 @@ import { eventBus } from "../../main";
 
 export default {
   props: ["tagArray"],
+  data() {
+    return {
+      isMounted: false
+    };
+  },
   methods: {
     categorySelected(index) {
       const category = this.tagArray[index].name;
       eventBus.updateCategory(category);
-      // const hexColor = this.colors[index].hex;
-      // const rgbColor = this.colors[index].rgb;
-      // eventBus.handleColorUpdate(hexColor, rgbColor);
+      const el = this.$refs[`tag-button-${index}`][0];
+      if ([...el.classList].includes("selected")) {
+        el.classList.remove("selected");
+        el.classList.remove("grow");
+      } else {
+        el.classList.add("grow");
+        setTimeout(() => {
+          el.classList.add("selected");
+        }, 268.75);
+      } //537.5
     }
   },
   mounted() {
-    console.log(this.tagArray);
+    this.isMounted = true;
   }
 };
 </script>
@@ -33,17 +46,57 @@ export default {
 @import '../../base.sass'
 
 ul
+  margin-top: 10px
   list-style: none 
   li 
-    display: inline
+    display: inline-block
     cursor: pointer
     font-family: $subfont
     font-size: .9em
-    border: 1px solid black 
+    border: 1px solid black
     margin: 0px 5px
     padding: 4px 15px
     border-radius: 100px
+    transition: all 0.3s ease
+    background: linear-gradient(to bottom, white 50%, black 50%)
+    background-size: 100% 200% 
+    background-position: right top
+    //box-shadow: inset 0px 0 0 0 #fff
+    &.grow 
+      animation: portfolio-tag-bounce .5s forwards
     &:nth-child(1)
       margin-left: 0px
+    &.selected 
+      color: white
+      background-position: right bottom
+
+
+@keyframes portfolio-tag-bounce 
+  from,
+  20%,
+  53%,
+  80%,
+  
+  to
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1)
+    transform: translate3d(0, 0, 0)
+
+  40%,
+  43%
+    animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06)
+    transform: translate3d(0, -15px, 0)
+
+  70%
+    animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06)
+    transform: translate3d(0, -7px, 0)
+
+
+  90%
+    transform: translate3d(0, -2px, 0)
+
+
+  
+
+  
 
 </style>
